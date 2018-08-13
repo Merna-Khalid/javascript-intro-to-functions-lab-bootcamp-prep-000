45a8549a19508cbf33e147811c412c66c1ede0c5
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-13 19:48:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   else if (shout(string) === string) {
     return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma.")) {
+  else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
