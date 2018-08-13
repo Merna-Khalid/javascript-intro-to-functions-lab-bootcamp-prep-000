29c91216c962e1f45f5c464acdb99ad19241cbe4
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-13 19:40:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
